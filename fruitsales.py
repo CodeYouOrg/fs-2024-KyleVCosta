bd8378c7f4527ba8c8a513d45afb579d5e7ffc6d
KyleVCosta <kylevictorc@gmail.com>
2024-09-22 03:20:42 -0400

    First Commit
    Created database df, and wrote it to fruit.csv.

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,9 @@
 # add your code here
+import pandas as pd
+
+df = pd.DataFrame({'Apples':[35,41], 'Bananas':[21,34]}, index = ['2017 Sales', '2018 Sales'])
+print(df)
+
+import csv
+
+df.to_csv('fruit.csv')
